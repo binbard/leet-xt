2264b76362a76c66c70186ecfef35e423820338f
DAVINCI\notok <harshitjawla123@gmail.com>
2023-09-10 20:22:33 +0530

    After index move changes for web

--- js/index.js
+++ js/index.js
@@ -1,23 +1,30 @@
 function showFeature(tabname) {
-    // Hide all features
     const features = document.querySelectorAll('.feature');
     features.forEach(feature => {
         feature.classList.add('hidden');
     });
 
-    // Show the selected feature
     const selectedFeature = document.getElementById(tabname);
     if (selectedFeature) {
         selectedFeature.classList.remove('hidden');
     }
 }
 
-function addClickHandlers(){
+function addClickHandlers() {
     document.querySelectorAll('ul.feature-list li').forEach((li) => {
         li.addEventListener('click', () => {
             showFeature(li.getAttribute('name'));
         });
     });
 }
+
+function initHandler() {
+    if (window.location.href.startsWith('http')) {
+        document.querySelector('.title-main').innerHTML = document.title;
+    }
+}
+
+
+initHandler();
 
 addClickHandlers();

00e6c55d7317786515a9156f307b2636d7c97a55
DAVINCI\notok <harshitjawla123@gmail.com>
2024-01-16 19:06:10 +0530

    Update buildscript

--- build.py
+++ build.py
@@ -2,21 +2,19 @@ import os
 import shutil
 import tempfile
 import sys
+import json
 
-items_to_zip = ['assets', 'css', 'icons', 'js', 'background.js', 'index.html', 'popup.html']
+items_to_zip = ['assets', 'css', 'html', 'js', 'index.html']
 
-manifest = 'manifest.json'
 manifest_firefox = 'manifest-firefox.json'
 manifest_chrome = 'manifest-chrome.json'
-manifest_source = ''
-if '-f' in sys.argv and os.path.isfile(manifest_firefox):
-    manifest_source = manifest_firefox
-elif os.path.isfile(manifest_chrome):
-    manifest_source = manifest_chrome
-else:
-    manifest_source = manifest
+manifest = 'manifest.json'
+created = False
 
-output_zip_filename = '../' + os.path.basename(os.getcwd()) + '.zip'
+
+output_zip_dir = '../versions'
+if not os.path.isdir(output_zip_dir):
+    os.mkdir(output_zip_dir)
 
 with tempfile.TemporaryDirectory() as temp_dir:
     for item in items_to_zip:
@@ -27,14 +25,23 @@ with tempfile.TemporaryDirectory() as temp_dir:
             shutil.copy(source_path, dest_path)
         elif os.path.isdir(source_path):
             shutil.copytree(source_path, dest_path)
+            
+    if os.path.isfile(manifest_chrome) or os.path.isfile(manifest):
+        manifest_source = maniefest_chrome if os.path.isfile(manifest_chrome) else manifest
+        shutil.copy(manifest_source, os.path.join(temp_dir, manifest))
+        version = json.load(open(manifest))['version']
+        output_zip_filename = f'{output_zip_dir}/{os.path.basename(os.getcwd())}-chrome-{version}'
+        shutil.make_archive(output_zip_filename, 'zip', temp_dir)
+        print(f'{output_zip_filename} for Chrome created successfully (using {manifest}).')
+        created = 1
+        
+    if os.path.isfile(manifest_firefox):
+        shutil.copy(manifest_firefox, os.path.join(temp_dir, manifest))
+        version = json.load(open(manifest_firefox))['version']
+        output_zip_filename = f'{output_zip_dir}/{os.path.basename(os.getcwd())}-firefox-{version}'
+        shutil.make_archive(output_zip_filename, 'zip', temp_dir)
+        print(f'{output_zip_filename} for Firefox created successfully (using {manifest_firefox}).')
+        created = 1
 
-    shutil.copy(manifest_source, os.path.join(temp_dir, manifest))
-
-    shutil.make_archive(output_zip_filename[:-4], 'zip', temp_dir)
-
-
-print(*items_to_zip, sep=', ')
-if manifest_source==manifest_firefox:
-    print(f'{output_zip_filename} for Firefox created successfully')
-else:
-    print(f'{output_zip_filename} for Chrome created successfully.')
+if not created:
+    print(f'Can\'t create a new version: No {manifest_chrome} or {manifest_firefox} Found')

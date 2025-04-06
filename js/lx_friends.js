97bdc10909717ef9a6900f0920b67596dc7ab9c4
binbard <harshitjawla123@gmail.com>
2025-04-06 21:37:30 +0530

    Navbar friend icon fix

--- js/lx_friends.js
+++ js/lx_friends.js
@@ -13,7 +13,8 @@ async function addFriendsIconOnNavbar() {
     svg.setAttribute('class', 'h-[20px] w-[20px] group-hover:text-text-primary dark:group-hover:text-dark-text-primary text-text-secondary dark:text-dark-text-secondary');
     svg.setAttribute('fill', 'currentColor');
 
-    let nua_root = navbar_user_avatar;
+    let nua_root = navbar_user_avatar?.parentElement?.parentElement?.parentElement?.parentElement;
+    if (!nua_root) return;
 
     nua_root.parentElement.insertBefore(a, nua_root);
 }

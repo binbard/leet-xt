cae2d719541d9f8fc34fe04650a69020279592df
DARSHAN <sdarshan.aids2023@citchennai.net>
2025-04-18 12:20:40 +0530

    [FIX]:Unneccesary popup of profile dialog fixed

--- src/core/modules/navbarFriendsButton.ts
+++ src/core/modules/navbarFriendsButton.ts
@@ -6,33 +6,37 @@ import Selectors from "@/values/selectors";
 import Manager from "../manager";
 
 export class NavbarFriendsButton implements IModule {
-
-    async action(_?: MutationRecord[], observer?: MutationObserver): Promise<void> {
-        try {
-
-            let navbar_user_avatar;
-
+     async action(_?: MutationRecord[], observer?: MutationObserver): Promise<void> {
             try {
-                navbar_user_avatar = docFind(Selectors.lc.static_dom.navbar.user_avatar);
-            } catch (e) {
-                // Manager.Logger.warn(NavbarFriendsButton.name, 'Navbar user avatar not found', e);
-                return;
+                // Manager.Logger.log("Darshan", NavbarFriendsButton.name);
+    
+                let navbar_user_avatar;
+    
+                try {
+                    navbar_user_avatar = docFind(Selectors.lc.static_dom.navbar.user_avatar);
+                } catch (e) {
+                    // Manager.Logger.warn(NavbarFriendsButton.name, 'Navbar user avatar not found', e);
+                    return;
+                }
+    
+                if (checkDone(navbar_user_avatar)) return;
+    
+                
+                const navbarFriendIcon = getNavbarFriendsIcon(PageType.Friends);
+                const button = navbar_user_avatar.closest('button');
+                const parent = button?.parentElement;
+                const grandparent = parent?.parentElement;
+    
+    
+                grandparent?.parentElement?.parentElement?.insertBefore(navbarFriendIcon, grandparent.parentElement);
+    
+                // observer?.disconnect();
+    
+                Manager.Logger.log("Completed", NavbarFriendsButton.name);
+            } catch (e: any) {
+                Manager.Logger.warn(NavbarFriendsButton.name, e);
             }
-
-            if (checkDone(navbar_user_avatar)) return;
-
-            const navbarFriendIcon = getNavbarFriendsIcon(PageType.Friends);
-
-            navbar_user_avatar?.parentElement?.insertBefore(navbarFriendIcon, navbar_user_avatar);
-
-            // observer?.disconnect();
-
-            Manager.Logger.log("Completed", NavbarFriendsButton.name);
-        } catch (e: any) {
-            Manager.Logger.warn(NavbarFriendsButton.name, e);
         }
-    }
-
     apply(): void {
         this.action();
         mutObserve(Selectors.lc.static_dom.navbar.self, this.action);

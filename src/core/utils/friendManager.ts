543d52358e11b422b3ab8601163effbf7aa0433c
binbard <harshitjawla123@gmail.com>
2025-04-21 14:54:58 +0530

    Old Friends not showing fix

--- src/core/utils/friendManager.ts
+++ src/core/utils/friendManager.ts
@@ -2,25 +2,40 @@ class FriendManager {
 
     static FRIENDS_LIMIT = 50;
     static FRIENDS_LOCATION: `local:${string}` = 'local:friends';
+    static FRIENDS_LOCATION_OLD: `local:${string}` = 'local:myfriends';
     static FRIENDS_MESSAGE = `FRIEND_LIMIT_EXCEEDED: You can only add upto ${this.FRIENDS_LIMIT} friends, ensuring an inclusive and sustainable experience for everyone!`;
     
+    static async getFriendList(): Promise<string[]> {
+        let friendList: string[] = await storage.getItem(FriendManager.FRIENDS_LOCATION, { fallback: [] });
+        
+        // Migrate
+        const friendListOld: string[] = await storage.getItem(FriendManager.FRIENDS_LOCATION_OLD, { fallback: [] });
+        if( friendListOld.length > 0 ) {
+            friendList = [...new Set([...friendList, ...friendListOld])];
+            await storage.setItem(FriendManager.FRIENDS_LOCATION, friendList);
+            await storage.removeItem(FriendManager.FRIENDS_LOCATION_OLD);
+        }
+
+        return friendList;
+    }
+
     static async isFriend(username: string): Promise<boolean> {
-        const friendList: string[] = await storage.getItem(FriendManager.FRIENDS_LOCATION, { fallback: [] });
+        const friendList: string[] = await this.getFriendList();
         return friendList.includes(username);
     }
 
     static async getFriends(): Promise<string[]> {
-        const friendList = await storage.getItem(FriendManager.FRIENDS_LOCATION, { fallback: [] });
+        const friendList = await this.getFriendList();
         return friendList;
     }
 
     static async getNumFriends(): Promise<number> {
-        const friendList = await storage.getItem(FriendManager.FRIENDS_LOCATION, { fallback: [] });
+        const friendList = await this.getFriendList();
         return friendList.length;
     }
 
     static async addFriend(username: string): Promise<void> {
-        let friendList: string[] = await storage.getItem(FriendManager.FRIENDS_LOCATION, { fallback: [] });
+        let friendList: string[] = await this.getFriendList();
 
         if (friendList.includes(username)) {
             return;
@@ -36,7 +51,7 @@ class FriendManager {
     }
 
     static async removeFriend(username: string): Promise<void> {
-        const friendList: string[] = await storage.getItem(FriendManager.FRIENDS_LOCATION, { fallback: [] });
+        const friendList: string[] = await this.getFriendList();
 
         if (!friendList.includes(username)) {
             return;

5a4f888bc3c0d57f41842d6291d346045003a0da
binbard <mail@binbard.org>
2025-04-21 14:40:31 +0530

    Friend + Profile navbar icon simultaneous focus fix

---
+++ src/values/selectors/lc/navbar.ts
@@ -0,0 +1,10 @@
+export default {
+    root: {
+        self: '#leetcode-navbar',
+        icon_container: {
+            self: '#leetcode-navbar > div.display-none.m-auto.w-full.items-center.justify-center.px-6 > div > div',
+            user_avatar: '#navbar_user_avatar',
+            profile_icon_container: '#leetcode-navbar > div.display-none.m-auto.w-full.items-center.justify-center.px-6 > div > div > div:nth-child(3)',
+        },
+    },
+}

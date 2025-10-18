5656c392555d95c712b2c0e8607f5e85665bc9a2
Bankim2410 <131542729+Bankim2410@users.noreply.github.com>
2025-10-19 01:12:41 +0530

    feat: Highlight friends in contest ranking page
    - Added color and highlighting for friends
    - Differentiated weekly and biweekly scheduling
    - Fixed file paths and removed redundant files
    - Improved user matching factor
    
    Co-authored-by: binbard <harshitjawla123@gmail.com>

--- src/values/config/colors.ts
+++ src/values/config/colors.ts
@@ -1,2 +1,15 @@
 export const FRIEND_STAR            = 'rgb(255, 226, 96)';              // #ffe260
 export const NOT_FRIEND_STAR        = 'rgb(204, 204, 215)';             // #ccccd7
+
+export enum ContestWeekly {
+    boxBorderColor = "#f5d76e",
+    boxLowerColor = "#492c1b",
+    boxUpperColor = "#211c1a",
+    boxBorderEndingColor = "#ab6c17"
+}
+export enum ContestBiweekly {
+    boxBorderColor = "#aff0ff",
+    boxLowerColor = "#281a4b",
+    boxUpperColor = "#181c24",
+    boxBorderEndingColor = "#2b61c8"
+}
